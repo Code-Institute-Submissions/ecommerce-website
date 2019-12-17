33008106d33d33b3a35ffbd746d3c58547737ca4
Eduardo Parkinson de Castro <eduardop@tcd.ie>
2019-12-17 18:07:35 +0000

    changed allowed hosts on heroku due to 404 error

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -33,7 +33,7 @@ DEBUG = os.environ.get('DEBUG', False)
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = [ 'https://e-commerce-10.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'https://e-commerce-10.herokuapp.com/']
 
 
 # Application definition
