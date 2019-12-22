db3196c01458565880ee29abe8da69bc077de743
Eduardo Parkinson de Castro <eduardop@tcd.ie>
2019-12-22 12:27:48 +0000

    changed AWS secret key and root key/user access key/ ignored some additional files includign chache files to prevent data leak

--- ecommerce/settings/dev.py
+++ ecommerce/settings/dev.py
@@ -1,15 +1,21 @@
 from .base import *
+from .local import *
 # importing os module  
 import os 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Python program to explain os.environ object  
-  
+  from django.utils.crypto import get_random_string
+
 
   
 # Add a new environment variable  
-os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAT2JWHGMKNZ53HD6P'
-os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAT2JWHGMKF2KX3BTF'
+os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
+
+
+chars = 'amnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+SECRET_KEY = get_random_string(50, chars)
